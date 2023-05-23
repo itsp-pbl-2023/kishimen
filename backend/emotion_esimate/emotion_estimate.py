56dfd40a4791e0188bbf94a7be4bc57e8bcbfebf
arakisota <soutaraki0126@gmail.com>
2023-05-23 16:34:15 +0900

    カメラで画像をキャプチャして、感情推定する

--- backend/emotion_esimate/emotion_estimate.py
+++ backend/emotion_esimate/emotion_estimate.py
@@ -1,14 +1,42 @@
 from fer import FER
 import matplotlib.pyplot as plt
 
-test_image_one = plt.imread("data/smile.py")
-emo_detector = FER(mtcnn=True)
-# Capture all the emotions on the image
-captured_emotions = emo_detector.detect_emotions(test_image_one)
-# Print all captured emotions with the image
-print(captured_emotions)
-plt.imshow(test_image_one)
+import cv2
 
-# Use the top Emotion() function to call for the dominant emotion in the image
-dominant_emotion, emotion_score = emo_detector.top_emotion(test_image_one)
-print(dominant_emotion, emotion_score)
+
+def capture_image(dst_path: str) -> None:
+    """
+    OpenCVを用いて、カメラで画像をキャプチャ
+    Args:
+        dst_path : 画像の出力パス
+    """
+    deviceid = 0  # it depends on the order of USB connection.
+    capture = cv2.VideoCapture(deviceid)
+
+    ret, frame = capture.read()
+    # cv2.imshow("test", frame)
+    cv2.imwrite(dst_path, frame)
+
+
+def emotion_estimate(image: str) -> None:
+    """
+    画像から感情推定する
+    Args:
+        image : 感情推定する画像のパス
+    """
+    test_image_one = plt.imread(image)
+    emo_detector = FER(mtcnn=True)
+    # Capture all the emotions on the image
+    captured_emotions = emo_detector.detect_emotions(test_image_one)
+    # Print all captured emotions with the image
+    print(captured_emotions)
+    plt.imshow(test_image_one)
+
+    dominant_emotion, emotion_score = emo_detector.top_emotion(test_image_one)
+    print(dominant_emotion, emotion_score)
+
+
+if __name__ == "__main__":
+    dst_path = "data/test.jpeg"
+    capture_image(dst_path)
+    emotion_estimate(dst_path)
