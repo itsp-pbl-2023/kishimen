73448d85ec3644393b44ecefa28d888e6ec06745
arakisota <soutaraki0126@gmail.com>
2023-05-30 14:22:45 +0900

    感情推定の結果を返すように指定

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
+from emotion_esimate.emotion_estimate import emotion_estimate
 
 router = APIRouter()
+
+image = "/workspace/kishimen/backend/emotion_esimate/data/sad.jpeg"
 
 
 @router.get("/")
 async def root():
-    return {"message": "Hello World"}
+    emotion = emotion_estimate(image)
+    return emotion
+
 
 app = FastAPI()
 app.include_router(router, prefix="/api")
