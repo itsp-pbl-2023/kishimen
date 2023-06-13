95ac30fb4439902595404114ee9b78562c4d6579
mazrean <shunsuke.w04@gmail.com>
2023-06-13 15:02:54 +0900

    カメラ映像取得

--- frontend/src/components/CapturedVideo.vue
+++ frontend/src/components/CapturedVideo.vue
@@ -1,10 +1,28 @@
 <template>
-  <video>
-    <source src="/test.mp4" />
-  </video>
+  <video ref="video" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const video = ref<HTMLVideoElement>()
+onMounted(() => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: false
+      })
+      .then(stream => {
+        if (video.value && video.value !== null) {
+          video.value.srcObject = stream
+          video.value.play()
+        }
+      })
+  }
+})
+</script>
+
 <style lang="scss" module>
 video {
   object-fit: contain;
